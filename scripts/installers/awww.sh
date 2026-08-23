ecdde19f23c9d69265a96189c8032d6a2db463ae
Ajani <ajani@cerebro.lan>
2026-08-22 21:29:43 -0400

    copy to local bin

--- scripts/installers/awww.sh
+++ scripts/installers/awww.sh
@@ -76,6 +76,13 @@ install_awww() {
 
     echo
     "$INSTALL_DIR/awww" --version
+    
+    echo "==> Coping to /usr/local/bin"
+    sudo chmod 755 /usr/local/bin/awww
+    sudo cp target/release/awww /usr/local/bin/awww
+
+    sudo chmod 755 /usr/local/bin/awww-daemon
+    sudo cp target/release/awww-daemon /usr/local/bin/awww-daemon
 }
 
 uninstall_awww() {
